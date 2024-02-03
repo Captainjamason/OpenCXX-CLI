4b5678e5a885fe1fa92ca78903592d609862e85d
Jamason Davis <jpdavis2471@gmail.com>
2024-02-02 23:04:49 -0600

    In progress CLI work.

---
+++ include/cli.h
@@ -0,0 +1,40 @@
+//  opencxx_cli
+//  cli.h
+//  Copyright 2024 - hurbIndustries
+//  JPD
+
+#ifndef cli_h
+#define cli_h
+
+#include <functional>
+#include <string>
+#include <vector>
+
+namespace opencxx_cli {
+    class CLI {
+        public: 
+
+        struct handler {
+            std::string version;
+            std::string name;
+            std::string author;
+        };
+
+        // Basic commands that *always* will work
+        std::string help(handler);
+        std::string ver(handler);
+
+        struct entry {
+            std::string lhand;
+            std::string shand;
+            std::function<int()> func;
+        };
+
+        std::vector<entry> entries;
+
+        // Add your own entry and handler
+        void addEntry(std::string lhand, std::string shand, std::function<int()> func);
+    };
+}
+
+#endif
---
+++ src/cli.cpp
@@ -0,0 +1,22 @@
+//  opencxx_cli
+//  cli.cpp
+//  Copyright 2024 - hurbIndustries
+//  JPD
+
+#include <iostream>
+#include <string>
+#include "../include/cli.h"
+
+using namespace opencxx_cli;
+
+void CLI::addEntry(std::string lhand, std::string shand, std::function<int ()> func) {
+    CLI::entry entry;
+    entry.lhand = lhand;
+    entry.shand = shand;
+    entry.func = func;
+    CLI::entries.push_back(entry);
+}
+
+std::string CLI::help(CLI::handler handler) {
+    std::string string = handler.name + "\n";
+}
