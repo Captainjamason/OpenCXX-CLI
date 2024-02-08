84177e1c8292d859c3588e71b4e750aec7a3d674
Jamason Davis <jpdavis2471@gmail.com>
2024-02-07 18:45:48 -0600

    cli.cpp gets comments...

--- src/cli.cpp
+++ src/cli.cpp
@@ -3,6 +3,8 @@
 //  Copyright 2024 - hurbIndustries
 //  JPD
 
+
+// Hopefully this section is fairly self explanatory...
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,6 +14,8 @@
 using namespace opencxx_cli;
 using namespace std;
 
+// Initialize the main 3 debug/error message commands, 
+// along with some nice formatting from the color library.
 void CLI::error(string s) {
     colors color;
     color.setColor(color.fgRed);
@@ -19,7 +23,6 @@ void CLI::error(string s) {
     color.reset();
     cout << s << "\n";
 }
-
 void CLI::warn(string s) {
     colors color;
     color.setColor(color.fgYellow);
@@ -27,15 +30,16 @@ void CLI::warn(string s) {
     color.reset();
     cout << s << "\n";
 }
-
- void CLI::info(string s) {
+void CLI::info(string s) {
     colors color;
     color.setColor(color.fgBlue);
     cout << "[INFO] ";
     color.reset();
     cout << s << "\n";
- }
+}
 
+// addEntry() will push a new entryData struct (cli.h) into an entries
+// vector, which is established application side and a vector is provided.
 void CLI::addEntry(string lhand, string shand, int(*func)(), vector<CLI::entryData> *entries) {
     CLI::entryData entry;
     entry.lhand = lhand;
@@ -44,6 +48,8 @@ void CLI::addEntry(string lhand, string shand, int(*func)(), vector<CLI::entryDa
     entries -> push_back(entry);
 }
 
+// Basic version command, This may not ever be called but is available for
+// implementation client side.
 void CLI::ver(struct programInfo info) {
     string output;
     output.append("[" + info.name + "]\n");
@@ -52,6 +58,8 @@ void CLI::ver(struct programInfo info) {
     cout << output;
 }
 
+// Vectorize allows for a quick function to take argc and argv[] and return
+// a vector of arguments... which is mainly used for parse().
 vector<string> CLI::vectorize(int argc, char *argv[]) {
     vector<string> args;
     // note: i = 1 due to 0 being the name of the executable itself. 
@@ -61,24 +69,35 @@ vector<string> CLI::vectorize(int argc, char *argv[]) {
     return args;
 }
 
+// TO-DO: Help command, This is hard coded into the library and will
+// always be available in the program. This will need to iterate over
+// a vector of the entries.
 void CLI::help(vector<CLI::entryData> entries) {
     cout << "[opencxx-cli] Help is under construction...\n";
     exit(1);
 }
 
+// Here's the big one, This will iterate and parse arguments, along with calling the
+// application side function...
 void CLI::parse(vector<CLI::entryData> entries, vector<string> args) {
+    // if no args then stop, no need to be wasting time.
     if(args.size() == 0) {
         error("No arguments provided... Please use --help");
         exit(1);
     }
+    // Iterate over args provided...
     for(int i = 0; i < args.size(); i++) {
+        // Hardcoded debugging command. Useful for developers...
         if(args[i] == "--opencxx-cli-debug") {
             debug = true;
+            info("OpenCXX-CLI is in debug mode. Arguments will be outputted to terminal...");
         }
         if(debug == true) {
             cout << "[ARGUMENT]=" << i << ": " << args[i] << "\n";
         }
 
+        // Check to see if its the help command (long or short), 
+        // Otherwise continue iterating
         if(args[i] == "--help" || args[i] == "-h") {
             help(entries);
         } else {
@@ -87,7 +106,10 @@ void CLI::parse(vector<CLI::entryData> entries, vector<string> args) {
                     entries[j].func();
                 } else if (args[i] == entries[j].lhand) {
                     entries[j].func();
-                } else if (args[i] != "--opencxx-cli-debug") {
+                } 
+                // This specific clause is needed for the debug to work
+                // without interrupting the program flow.
+                else if (args[i] != "--opencxx-cli-debug") {
                     error("Invalid arguments... Please use --help");
                     exit(1);
                 }  
