29bf6bf9cd43e2f10696aa78a54665d48b6ad5e8
Jamason Davis <jpdavis2471@gmail.com>
2024-02-08 10:31:32 -0600

    Improved unit tests.

--- src/unitTests.cpp
+++ src/unitTests.cpp
@@ -17,48 +17,71 @@ int testFunc() {
 
 int main(int argc, char* argv[]) {
     CLI cli;
+    std::string test = argv[1];
+
     // test output messages...
-    if(cli.error("testerr") != 0) {
-        std::cout << "cli.error() failed!\n";
-        return 1;
+    if(test == "-err") {
+        if(cli.error("testerr") != 0) {
+            std::cout << "cli.error() failed!\n";
+            return 1;
+        } else {
+            return 0;
+        }
     }
-    if(cli.warn("testwarn") != 0) {
-        std::cout << "cli.warn() failed!\n";
-        return 1;
+    if(test == "-warn") {
+        if(cli.warn("testwarn") != 0) {
+            std::cout << "cli.warn() failed!\n";
+            return 1;
+        }
     }
-    if(cli.info("testinfo") != 0) {
-        std::cout << "cli.info() failed!\n";
-        return 1;
+    if(test == "-info") {
+        if(cli.info("testinfo") != 0) {
+            std::cout << "cli.info() failed!\n";
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    if(test == "-addEntryOnce") {
+        if(cli.addEntry("--test", "-t", testFunc, &entries)) {
+            return 0;
+            entries[0].func();
+        } else {
+            return 1;
+        }
     }
 
     //test add entry
-    if(cli.addEntry("--test", "-t", testFunc, &entries)) {
-        if(entries[0].func != 0) {
-            std::cout << "err: entries not stored successfully...\n";
+    if(test == "-addEntryTwice") {
+        if(cli.addEntry("test1", "t1", testFunc, &entries)) {
+            entries[0].func();
+            if(cli.addEntry("test2", "t2", testFunc, &entries)) {
+                entries[1].func();
+            } else {
+                return 1;
+            }
+            return 0;
+        } else {
             return 1;
         }
-        // Test mutliple entries!
-        if(cli.addEntry("--test", "-t", testFunc, &entries)) {
-            if(entries[1].func != 0) {
-                std::cout << "err: multiple entries not working...\n";
-                return 1;
-            }
-        }
+    }
 
-        // Test parsing...
-        if(argv[1] != "") {
-            if(cli.parse(entries, cli.vectorize(argc, argv)) != 0) {
-                std::cout << "err: parse() failed!\n";
-                return 1;
-            }
+    if(test == "-testParse") {
+        cli.addEntry("--test", "-t", testFunc, &entries);
+        cli.addEntry("--t2", "-t2", testFunc, &entries);
+        if(cli.parse(entries, cli.vectorize(argc, argv))) {
+            return 0;
         } else {
-            std::cout << "info: no args passed, skipping parse()\n";
+            return 1;
         }
     }
 
     // test help()
-    if(cli.help(entries) != 0) {
-        std::cout << "err: help() failed!\n";
-        return 1;
+    if(test == "-help") {
+        if(cli.help(entries) != 0) {
+            std::cout << "err: help() failed!\n";
+            return 1;
+        }
     }
 }
