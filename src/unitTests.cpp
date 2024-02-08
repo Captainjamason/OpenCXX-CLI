60975eccdbdd75ee71902ddf3f58acc29bb56835
Jamason Davis <jpdavis2471@gmail.com>
2024-02-08 08:52:25 -0600

    Starting unitTest development

---
+++ src/unitTests.cpp
@@ -0,0 +1,17 @@
+//  opencxx_cli
+//  unitTests.cpp
+//  Copyright 2024 - hurbIndustries
+//  JPD
+
+#include <vector>
+#include "../include/cli.h"
+
+// Some basic initialization...
+using namespace opencxx_cli;
+CLI cli;
+std::vector<CLI::entryData> entries;
+
+int main() {
+    // test output messages...
+    if()
+}
