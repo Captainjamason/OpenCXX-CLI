3970df29ff37be7ff97f1b510ecd50f72b389dd9
Jamason Davis <jpdavis2471@gmail.com>
2024-11-17 00:20:13 -0600

    Working nix derivation, No devenv over here.

---
+++ builder.sh
@@ -0,0 +1,9 @@
+
+##  opencxx_cli
+##  install.sh
+##  Copyright 2024 - hurbIndustries
+##  JPD
+
+
+meson setup build
+#meson compile -C build 
--- default.nix
+++ default.nix
@@ -1,9 +1,33 @@
 {pkgs ? import <nixpkgs> {} }:
 pkgs.stdenv.mkDerivation {
     name = "opencxx-cli";
-    src = ./out;
+    src = ./.;
+
+    dontUnpack = true;
+    dontUseMesonConfigure = true;
+
+    PATH = "${pkgs.meson}/bin:${pkgs.ninja}/bin";
+
+    nativeBuildInputs = [
+        pkgs.meson
+        pkgs.ninja
+        pkgs.pkg-config
+    ];
+
+    configurePhase = "
+        meson setup $src $TMPDIR/build
+    ";
+
+    buildPhase = "
+        meson compile -C $TMPDIR/build
+    ";
 
     installPhase = "
-	cp -rv $src $out
+        mkdir -p $out/lib
+	    cp -rv $TMPDIR/build/src/libopencxx-cli.so $out/lib/
+        mkdir -p $out/include
+        cp -rv $src/include $out/include
+        cp -rv $TMPDIR/build/meson-private/opencxx-cli.pc $out/lib
     ";
 }
+
